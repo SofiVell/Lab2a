e1c2d1d133bed14e56617ae56f206db084fde550
SofiVell <sofivell7@gmail.com>
2025-10-21 12:32:01 +0300

    Add files via upload

---
+++ Lab2a/Lab2a/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Введіть ділене (перше ціле число):");
+        int dividend = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введіть дільник (друге ціле число):");
+        int divisor = Convert.ToInt32(Console.ReadLine());
+
+        if (divisor != 0)
+        {
+            int quotient = dividend / divisor;   
+            int remainder = dividend % divisor;  
+
+            Console.WriteLine($"Результат ділення: {dividend} / {divisor} = {quotient}");
+            Console.WriteLine($"Залишок від ділення: {remainder}");
+        }
+        else
+        {
+            Console.WriteLine("Помилка: ділення на 0 неможливе!");
+        }
+    }
+}
